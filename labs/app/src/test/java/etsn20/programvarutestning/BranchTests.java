366dd71388059a134bad1085a029718f2ed259cf
jacobperssons <jacob.persson@spiideo.com>
2023-11-08 09:52:06 +0100

    Add Test Setup

--- labs/app/src/test/java/etsn20/programvarutestning/BranchTests.java
+++ labs/app/src/test/java/etsn20/programvarutestning/BranchTests.java
@@ -1,5 +1,15 @@
 package etsn20.programvarutestning;
 
+import org.junit.jupiter.api.BeforeAll;
+
 class BranchTests {
-    
+    private static NextDate nextDate;
+
+    private static String INVALID_INPUT = "Invalid Input Date",
+                          INVALID_YEAR  = "Invalid Next Year";
+
+    @BeforeAll
+    static void setup() {
+        nextDate = new NextDate(0, 0, 0);
+    }
 }
--- labs/app/src/test/java/etsn20/programvarutestning/PredicateTests.java
+++ labs/app/src/test/java/etsn20/programvarutestning/PredicateTests.java
@@ -8,7 +8,10 @@ import org.junit.jupiter.api.Test;
 class PredicateTests {
 
     private static NextDate nextDate;
-    
+
+    private static String INVALID_INPUT = "Invalid Input Date",
+                          INVALID_YEAR  = "Invalid Next Year";
+
     @BeforeAll
     static void setup() {
         nextDate = new NextDate(0, 0, 0);
