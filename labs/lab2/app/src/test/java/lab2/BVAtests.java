bf21708f175f6759a3317f4bce3fac667cbd3570
oliver.lundgren <oliver.lundgren@spiideo.com>
2023-11-15 17:01:49 +0100

    Add tests for Boundary

--- labs/lab2/app/src/test/java/lab2/BVAtests.java
+++ labs/lab2/app/src/test/java/lab2/BVAtests.java
@@ -49,6 +49,124 @@ public class BVAtests {
 		triangle = null;
 	}
 
+    /*---------------------------------Equal Sides---------------------------------*/
+
+	@Test
+    public void testBVA_whenAllSidesAreEqual_thenReturnValid() {
+        triangle.setSideLengths(80, 80, 80);
+
+        getSideLengthsOfTriangle("80,80,80");
+        getAreaOfTriangle(2771.3);
+        getPerimeterOfTriangle(240);
+        classifyTriangle("equilateral");
+
+        assertTrue(triangle.isEquilateral());
+    }
+
+	@Test
+    public void testBVA_whenAllSidesAreEqual_thenReturnInvalid() {
+        triangle.setSideLengths(80, 80, 79);
+
+        getSideLengthsOfTriangle("80,80,79");
+
+        assertFalse(triangle.isEquilateral());
+    }
+
+
+	@Test
+    public void testBVA_whenTwoSidesAreEqual_thenReturnValid() {
+        triangle.setSideLengths(80, 80, 79);
+
+        getSideLengthsOfTriangle("80,80,79");
+        getAreaOfTriangle(2747.9);
+        getPerimeterOfTriangle(239);
+        classifyTriangle("isosceles");
+
+        assertTrue(triangle.isIsosceles());
+    }
+
+	@Test
+    public void testBVA_whenTwoSidesAreEqual_thenReturnInvalid() {
+        triangle.setSideLengths(79, 80, 81);
+
+        getSideLengthsOfTriangle("79,80,81");
+
+        assertFalse(triangle.isIsosceles());
+    }
+
+
+    /*---------------------------------Right Angle---------------------------------*/
+
+
+	@Test
+    public void testBVA_whenRightAngle_thenReturnValid() {
+        triangle.setSideLengths(30, 40,	50);
+
+        getSideLengthsOfTriangle("30,40,50");
+        getAreaOfTriangle(600);
+        getPerimeterOfTriangle(120);
+        classifyTriangle("right-angled");
+
+        assertTrue(triangle.isRightAngled());
+    }
+
+	@Test
+    public void testBVA_whenRightAngle_thenReturnInvalid() {
+        triangle.setSideLengths(30, 40, 51);
+
+        getSideLengthsOfTriangle("30,40,51");
+
+        assertFalse(triangle.isRightAngled());
+	}
+
+    /*---------------------------------Scalene---------------------------------*/
+
+
+	@Test
+    public void testBVA_whenAllRandomSides_thenReturnValid() {
+        triangle.setSideLengths(112, 102, 140);
+
+        getSideLengthsOfTriangle("124,102,140");
+        getAreaOfTriangle(6132.4);
+        getPerimeterOfTriangle(366);
+        classifyTriangle("scalene");
+
+        assertTrue(triangle.isScalene());
+    }
+
+	@Test
+    public void testBVA_whenAllRandomSides_thenReturnInvalid() {
+        triangle.setSideLengths(112, 112, 189);
+
+        getSideLengthsOfTriangle("112,112,189");
+
+        assertFalse(triangle.isScalene());
+    }
+
+    /*---------------------------------Impossible---------------------------------*/
+
+
+	@Test
+    public void testBVA_whenImpossible_thenReturnValid() {
+        triangle.setSideLengths(100, 101, 200);
+
+        getSideLengthsOfTriangle("100,101,200");
+        getAreaOfTriangle(1001.2);
+        getPerimeterOfTriangle(401);
+        classifyTriangle("scalene");
+
+        assertFalse(triangle.isImpossible());
+    }
+
+	@Test
+    public void testBVA_whenAllRandomSides_thenReturnInvalid() {
+        triangle.setSideLengths(112, 112, 189);
+
+        getSideLengthsOfTriangle("112,112,189");
+
+        assertFalse(triangle.isImpossible());
+    }
+
 	/* 
 	 * Tests whether the triangle specified in the fixture (setUp) 
 	 * is right-angled. 	
@@ -61,5 +179,23 @@ public class BVAtests {
 	*/
     void dummyTest() {
         assertTrue("This is a dummy test", false);
+    }
+
+
+    /*---------------------------------HELPER METHODS---------------------------------*/
+    void classifyTriangle(String expected) {
+        assertEquals(expected, triangle.classify());
+    }
+
+    void getAreaOfTriangle(double expected) {
+        assertEquals(expected, triangle.getArea(), 0.1);
+    }
+
+    void getPerimeterOfTriangle(int expected) {
+        assertEquals(expected, triangle.getPerimeter());
+    }
+
+    void getSideLengthsOfTriangle(String expected) {
+        assertEquals(expected, triangle.getSideLengths());
     }
 }
